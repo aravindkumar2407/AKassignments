80692c900b51b0ff5c49285aa33d9a66bcb19204
aravindkumar2407 <147939699+aravindkumar2407@users.noreply.github.com>
2023-10-19 21:13:04 +0530

    Add files via upload
    week1

---
+++ week1/day1/Browser.java
@@ -0,0 +1,33 @@
+package week1.day1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// data type  variable name = value
+		//browser version
+		float browserVersion = 10;
+		
+		//browsername
+		String browserName = "Firefox";
+		
+		//Pageloaded
+		boolean pageLoaded = true;
+		
+		// No of browser
+		int noOfbrowser = 30;
+		
+		// Browserlogo
+		char browserLogo = 'A';
+			
+				System.out.println(browserVersion);
+				System.out.println(browserName);
+				System.out.println(pageLoaded);
+				System.out.println(noOfbrowser);
+				System.out.println(browserLogo);
+		
+				
+	}
+
+}
