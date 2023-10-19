9bbced5634c131cc54791aacc7ca9bbae749a515
aravindkumar2407 <147939699+aravindkumar2407@users.noreply.github.com>
2023-10-19 21:30:06 +0530

    Add files via upload
    Week1 assignments

---
+++ week1/day1/FindNumber.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 45;
+		
+		if (a>0) {
+			
+			System.out.println("The number is positive");
+		}
+		
+		else {
+			
+			System.out.println("The number is negative");
+		}
+			
+			
+		}
+
+	}
